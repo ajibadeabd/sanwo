fa946321530bf9d855d37bd7097ebf9d16526456
Natterbase <tech@natterbase.com>
2018-12-10 13:18:23 +0000

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,8 @@
 const Sample = require('./sampleModel')
 const Inventory = require('./inventoryModel')
 const Order = require('./orderModel')
-<<<<<<< HEAD
 const Gift = require('./giftCardModel')
 const Wallet = require('./walletModel')
-
-module.exports = {
-  Sample, Inventory, Order, Gift, Wallet 
-=======
 const User = require('./userModel')
 
 
@@ -15,7 +10,7 @@ module.exports = {
   Sample, 
   Inventory, 
   Order, 
-  User,
-  
->>>>>>> 7e055b6c998483f2c3607422c0be90fd549a8194
+  Gift, 
+  Wallet,
+  User
 }
