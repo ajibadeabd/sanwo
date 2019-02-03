36d25d92061a0ea6ce780f1fc7c724ffd9eab644
Balogun Wahab <03balogun@gmail.com>
2019-02-03 12:29:40 +0100

    orders

---
+++ src/models/purchaseModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+
+const { Schema } = mongoose
+
+const CartSchema = new Schema({
+  product: {
+    type: Schema.ObjectId,
+    ref: 'Inventory',
+    required: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    minimum: 1,
+  },
+  installmentPeriod: {
+    type: Number,
+  },
+  installmentPercentage: {
+    type: Number,
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+  },
+  subTotal: {
+    type: Number,
+    required: true,
+  },
+  installmentInterest: {
+    type: Number,
+  },
+  installmentTotalRepayment: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+CartSchema.statics = {
+  valueExists (query) {
+    return this.findOne(query)
+      .then(result => result)
+  }
+}
+
+module.exports = {
+  Model: mongoose.model('Cart', CartSchema),
+  CartSchema
+}
