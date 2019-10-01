e493bd09837a1646cd81217bbd277b7c17b12969
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-10-01 09:46:59 +0100

    🐛 FIX: status code for update cooperate profile

--- src/controllers/cooperativeController.js
+++ src/controllers/cooperativeController.js
@@ -44,7 +44,7 @@ const update = async (req, res) => {
     }
 
     const updatedUser = await userExist.save()
-    res.send({
+    res.status(200).send({
       success: true,
       message: 'Updated successfully',
       data: updatedUser,
