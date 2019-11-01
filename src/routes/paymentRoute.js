c0aba34c2adb3a26eff228c259974162c599d6e7
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2019-11-01 14:28:07 +0100

    go

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -1,30 +1,41 @@
-const express = require('express')
-const paymentValidationMiddleware = require('./../functions/paymentValidationMiddleware')
-const authMiddleware = require('./../functions/authMiddleware')
+const express = require("express");
+const paymentValidationMiddleware = require("./../functions/paymentValidationMiddleware");
+const authMiddleware = require("./../functions/authMiddleware");
 
-const paymentController = require('../controllers/paymentController')
+const paymentController = require("../controllers/paymentController");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/processed', paymentController.notification)
-router.post('/debit-notification', paymentController.debitNotification)
-router.use(authMiddleware.isAuthenticated)
+router.get("/processed", paymentController.notification);
+router.get(
+  "/mandate-status/:orderId",
+  paymentController.installmentMandateStatus
+);
+router.post("/debit-notification", paymentController.debitNotification);
 
-router.post('/order', paymentValidationMiddleware.generateOrderPaymentRRR,
-  paymentController.generateOrderPaymentRRR)
+router.get(
+  "/order/:orderNumber",
+  paymentValidationMiddleware.getOrderPayments,
+  paymentController.getOrderPayments
+);
 
-router.get('/order/:orderNumber', paymentValidationMiddleware.getOrderPayments,
-  paymentController.getOrderPayments)
+router.get(
+  "/installment-payment-history/:orderId",
+  paymentValidationMiddleware.installmentMandateStatus,
+  paymentController.installmentPaymentHistory
+);
 
+router.get(
+  "/:paymentId",
+  paymentValidationMiddleware.getPayment,
+  paymentController.getPayment
+);
 
-router.get('/mandate-status/:orderId', paymentValidationMiddleware.installmentMandateStatus,
-  paymentController.installmentMandateStatus)
+router.use(authMiddleware.isAuthenticated);
 
-router.get('/installment-payment-history/:orderId', paymentValidationMiddleware.installmentMandateStatus,
-  paymentController.installmentPaymentHistory)
-
-router.get('/:paymentId', paymentValidationMiddleware.getPayment,
-  paymentController.getPayment)
-
-
-module.exports = router
+router.post(
+  "/order",
+  paymentValidationMiddleware.generateOrderPaymentRRR,
+  paymentController.generateOrderPaymentRRR
+);
+module.exports = router;
